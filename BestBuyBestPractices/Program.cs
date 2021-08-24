ddc42896765dd558d610911da876c3bd84bfc3f5
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-08-24 17:44:00 -0600

    Add project files.

---
+++ BestBuyBestPractices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyBestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
